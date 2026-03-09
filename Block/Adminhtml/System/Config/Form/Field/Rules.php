c0e053fdb2b280f55616f040f0844c58edfc984d
linxok <linxok@gmail.com>
2026-03-09 21:43:29 +0200

    design

--- Block/Adminhtml/System/Config/Form/Field/Rules.php
+++ Block/Adminhtml/System/Config/Form/Field/Rules.php
@@ -2,17 +2,52 @@
 namespace MyCompany\CustomerBlocklist\Block\Adminhtml\System\Config\Form\Field;
 
 use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+use Magento\Framework\Data\Form\Element\AbstractElement;
 
 class Rules extends AbstractFieldArray
 {
     protected function _prepareToRender()
     {
-        $this->addColumn('email', ['label' => __('Email')]);
-        $this->addColumn('telephone', ['label' => __('Telephone')]);
-        $this->addColumn('firstname', ['label' => __('First Name')]);
-        $this->addColumn('lastname', ['label' => __('Last Name')]);
-        $this->addColumn('note', ['label' => __('Note')]);
+        $this->addColumn('email', ['label' => __('Email'), 'style' => 'width: 260px;', 'size' => '40']);
+        $this->addColumn('telephone', ['label' => __('Telephone'), 'style' => 'width: 180px;', 'size' => '22']);
+        $this->addColumn('firstname', ['label' => __('First Name'), 'style' => 'width: 180px;', 'size' => '22']);
+        $this->addColumn('lastname', ['label' => __('Last Name'), 'style' => 'width: 180px;', 'size' => '22']);
+        $this->addColumn('note', ['label' => __('Note'), 'style' => 'width: 320px;', 'size' => '50']);
         $this->_addAfter = false;
         $this->_addButtonLabel = __('Add Rule');
+    }
+
+    public function render(AbstractElement $element)
+    {
+        $isCheckboxRequired = $this->_isInheritCheckboxRequired($element);
+
+        if ($element->getInherit() == 1 && $isCheckboxRequired) {
+            $element->setDisabled(true);
+        }
+
+        if ($element->getIsDisableInheritance()) {
+            $element->setReadonly(true);
+        }
+
+        $html = '<td class="label"></td>';
+        $html .= '<td class="value">';
+        $html .= '<div class="admin__field-label" style="float:none; width:auto; text-align:left; margin-bottom:8px; padding:0;">';
+        $html .= '<label for="' . $element->getHtmlId() . '"><span' . $this->_renderScopeLabel($element) . '>' . $element->getLabel() . '</span></label>';
+        $html .= '</div>';
+        $html .= $this->_getElementHtml($element);
+
+        if ($element->getComment()) {
+            $html .= '<p class="note"><span>' . $element->getComment() . '</span></p>';
+        }
+
+        $html .= '</td>';
+
+        if ($isCheckboxRequired) {
+            $html .= $this->_renderInheritCheckbox($element);
+        }
+
+        $html .= $this->_renderHint($element);
+
+        return $this->_decorateRowHtml($element, $html);
     }
 }
